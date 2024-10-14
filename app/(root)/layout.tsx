f608202aa938486e89ad3e3a766ef6232bdb63b4
Sinem <sinemcann6@gmail.com>
2024-10-14 19:12:53 +0300

    next pg app

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,16 +4,16 @@ import React from 'react'
 
 export default function RootLayout({
   children,
-  modal,
-}: {
+}: //modal,
+{
   children: React.ReactNode
-  modal: React.ReactNode
+  //modal: React.ReactNode
 }) {
   return (
     <div className="flex h-screen flex-col">
       <Header />
       <main className="flex-1 wrapper">{children}</main>
-      {modal}
+
       <Footer />
     </div>
   )
