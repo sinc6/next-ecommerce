1f9058b751b567836239dd48340a4f9e3ae92e3d
Sinem <sinemcann6@gmail.com>
2024-10-15 22:06:35 +0300

    sign-in sign-up

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -2,8 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { APP_NAME } from '@/lib/constants'
 
-import { Button } from '@/components/ui/button'
-import { ShoppingCart, UserIcon } from 'lucide-react'
+import Menu from './menu'
 
 const Header = async () => {
   return (
@@ -20,20 +19,7 @@ const Header = async () => {
             {APP_NAME}
           </Link>
         </div>
-        <div className="space-x-2">
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingCart />
-              Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/cart">
-              <UserIcon />
-              Sign In
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   )
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,11 +1,19 @@
 'use server'
-import { desc } from 'drizzle-orm'
+import { desc } from 'drizzle-orm/sql'
 import db from '@/db/drizzle'
 import { products } from '@/db/schema'
+import { eq } from 'drizzle-orm/sql'
+
 export async function getLatestProducts() {
   const data = await db.query.products.findMany({
     orderBy: [desc(products.createdAt)],
     limit: 4,
   })
   return data
+}
+
+export async function getProductBySlug(slug: string) {
+  return await db.query.products.findFirst({
+    where: eq(products.slug, slug),
+  })
 }
